441adc27b5922c073765248ef76b458eeff511e9
Vadim Zhamkov <vad-js@nb-nq21tq2j95.barn-sun.ts.net>
2024-02-08 10:46:17 +0100

    Initial commit

---
+++ apps/dummy/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'dummy',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/dummy',
+};
